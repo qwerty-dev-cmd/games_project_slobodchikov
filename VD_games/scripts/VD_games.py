18f54145e200455535ea14d3da96d8ed1c3682e8
qwerty-dev-cmd <student@PC.localdomain>
2026-02-17 14:25:05 +0300

    Удалены старые файлы

--- VD_games/scripts/VD_games.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Модуль VD_games запущен")
-
-if __name__ == "__main__":
-    main()
--- VD_games/scripts/VD_main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Привет из модуля VD_main")
-
-if __name__ == "__main__":
-    main()
